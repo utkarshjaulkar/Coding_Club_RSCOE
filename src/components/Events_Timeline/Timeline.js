ccf5f02ce3e4d29073718a7dcd37496154670a6c
Utkarsh Jaulkar <ujaulkar1604@gmail.com>
2022-11-10 23:39:09 +0530

    Changed faculty infoand UI

--- src/components/Events_Timeline/Timeline.js
+++ src/components/Events_Timeline/Timeline.js
@@ -1,11 +1,11 @@
 import {
   VerticalTimeline,
-  VerticalTimelineElement
-} from "react-vertical-timeline-component";
-import "react-vertical-timeline-component/style.min.css";
-import Footer from "../Footer/Footer";
-import Navbar from "../Navbar/Navbar";
-import "./Timeline.css";
+  VerticalTimelineElement,
+} from 'react-vertical-timeline-component'
+import 'react-vertical-timeline-component/style.min.css'
+import Footer from '../Footer/Footer'
+import Navbar from '../Navbar/Navbar'
+import './Timeline.css'
 
 const Timeline = () => {
   return (
@@ -15,98 +15,73 @@ const Timeline = () => {
       <div className="timeline-title">
         <h2> Coding Club RSCOE Events Timeline</h2>
       </div>
+
       <div className="timeline">
         <VerticalTimeline>
           <VerticalTimelineElement
             className="vertical-timeline-element--work"
-            contentStyle={{ background: "rgb(33, 150, 243)", color: "#fff" }}
-            contentArrowStyle={{ borderRight: "7px solid  rgb(33, 150, 243)" }}
-           
-            iconStyle={{ background: "rgb(33, 150, 43)", color: "#fff" }}
+            contentStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+            contentArrowStyle={{ borderRight: '7px solid  rgb(33, 150, 243)' }}
+            iconStyle={{ background: 'rgb(33, 150, 43)', color: '#fff' }}
           >
-            <h3 className="vertical-timeline-element-title">
-              Events timeline
-            </h3>
+            <h3 className="vertical-timeline-element-title">Events timeline</h3>
             <h4 className="vertical-timeline-element-subtitle"></h4>
-            <p>
-             Timeline of events
-            </p>
+            <p>Timeline of events</p>
           </VerticalTimelineElement>
           <VerticalTimelineElement
             className="vertical-timeline-element--work"
             date="Month Year"
-            iconStyle={{ background: "rgb(33, 150, 243)", color: "#fff" }}
+            iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
           >
             <h3 className="vertical-timeline-element-title"></h3>
-            <h4 className="vertical-timeline-element-subtitle">
-              Event 5
-            </h4>
-            <p>
-              Details of Event
-            </p>
+            <h4 className="vertical-timeline-element-subtitle">Event 5</h4>
+            <p>Details of Event</p>
           </VerticalTimelineElement>
           <VerticalTimelineElement
             className="vertical-timeline-element--work"
             date="Month Year"
-            iconStyle={{ background: "rgb(33, 150, 243)", color: "#fff" }}
+            iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
           >
             <h3 className="vertical-timeline-element-title"></h3>
-            <h4 className="vertical-timeline-element-subtitle">
-              Event 4
-            </h4>
-            <p>
-              Details of Event
-            </p>
+            <h4 className="vertical-timeline-element-subtitle">Event 4</h4>
+            <p>Details of Event</p>
           </VerticalTimelineElement>
           <VerticalTimelineElement
             className="vertical-timeline-element--work"
             date="Month Year"
-            iconStyle={{ background: "rgb(33, 150, 243)", color: "#fff" }}
+            iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
           >
             <h3 className="vertical-timeline-element-title"></h3>
-            <h4 className="vertical-timeline-element-subtitle">
-              Event 3
-            </h4>
-            <p>
-              Details of Event
-            </p>
+            <h4 className="vertical-timeline-element-subtitle">Event 3</h4>
+            <p>Details of Event</p>
           </VerticalTimelineElement>
           <VerticalTimelineElement
             className="vertical-timeline-element--work"
             date="Month Year"
-            iconStyle={{ background: "rgb(33, 150, 243)", color: "#fff" }}
+            iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
           >
             <h3 className="vertical-timeline-element-title"></h3>
-            <h4 className="vertical-timeline-element-subtitle">
-              Event 2
-            </h4>
-            <p>
-              Details of Event
-            </p>
+            <h4 className="vertical-timeline-element-subtitle">Event 2</h4>
+            <p>Details of Event</p>
           </VerticalTimelineElement>
           <VerticalTimelineElement
             className="vertical-timeline-element--work"
             date="Month Year"
-            iconStyle={{ background: "rgb(33, 150, 243)", color: "#fff" }}
+            iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
           >
             <h3 className="vertical-timeline-element-title"></h3>
-            <h4 className="vertical-timeline-element-subtitle">
-              Event 1
-            </h4>
-            <p>
-              Details of Event
-            </p>
+            <h4 className="vertical-timeline-element-subtitle">Event 1</h4>
+            <p>Details of Event</p>
           </VerticalTimelineElement>
-          
-          
+
           <VerticalTimelineElement
-            iconStyle={{ background: "rgb(16, 204, 82)", color: "#fff" }}
+            iconStyle={{ background: 'rgb(16, 204, 82)', color: '#fff' }}
           />
         </VerticalTimeline>
       </div>
       <Footer />
     </>
-  );
-};
+  )
+}
 
-export default Timeline;
+export default Timeline
--- src/components/Faculty/Faculty.js
+++ src/components/Faculty/Faculty.js
@@ -1,8 +1,7 @@
-import Navbar from "../Navbar/Navbar";
-import Profile from "../utilities/ProfileCard/Profile";
-import Footer from "../Footer/Footer";
-import "./Faculty.css";
-import "animate.css";
+import Navbar from '../Navbar/Navbar'
+import Footer from '../Footer/Footer'
+import './Faculty.css'
+import 'animate.css'
 
 const Faculty = () => {
   return (
@@ -10,16 +9,36 @@ const Faculty = () => {
       <Navbar />
 
       <div className="card-profile">
-        <div className="col-12 col-sm-6 col-md-4 col-lg-3 animate__animated animate__bounceInLeft">
-          <Profile name="Prof.Avinash Golande" position="" />
+        <div class="profile-div-1 animate__animated animate__bounceInLeft">
+          <div class="our-team ">
+            <div class="picture">
+              <img class="img-fluid" src="./team-images/Avinash-sir.png" />
+            </div>
+            <div class="team-content">
+              <h3 class="name">Prof. Avinash Golande</h3>
+              <h4 class="title">Assistant Professor</h4>
+            </div>
+            <ul class="social"> </ul>
+          </div>
         </div>
-        <div className="col-12 col-sm-6 col-md-4 col-lg-3 animate__animated animate__bounceInRight">
-          <Profile name="Prof.Amruta Hingmire" position="" />
+        <div class="profile-div-1 animate__animated animate__bounceInRight">
+          <div class="our-team ">
+            <div class="picture">
+              <img class="img-fluid" src="./team-images/amruta-mam.png" />
+            </div>
+            <div class="team-content">
+              <h3 class="name">Prof. Amruta Hingmire</h3>
+              <h4 class="title">Assistant Professor</h4>
+            </div>
+            <ul class="social"> </ul>
+          </div>
         </div>
+
+        <div className="col-12 col-sm-6 col-md-4 col-lg-3 animate__animated animate__bounceInRight"></div>
       </div>
       <Footer />
     </>
-  );
-};
+  )
+}
 
-export default Faculty;
+export default Faculty
